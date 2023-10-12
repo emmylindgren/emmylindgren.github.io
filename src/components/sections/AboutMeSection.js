fcbe8010ab1737016a0b9e9172603ecf0698a339
Emmy Lindgren <emmy.lindgren@hotmail.com>
2023-10-12 19:50:30 +0200

    About me section done. Some cleanup.

--- src/components/sections/AboutMeSection.js
+++ src/components/sections/AboutMeSection.js
@@ -4,14 +4,17 @@ import styles from './AboutMeSection.module.css';
 
 function AboutMeSection() {
   return (
-    <div className='mainContainer' id='aboutMeSection'>
+    <div className={'mainContainer ' + styles.AboutMeSection}  id='aboutMeSection'>
         <h2 className='sectionHeading'>Om mig</h2>
         <div className={styles.container}>
             <div className={styles.innerContainer}>
                 <div className={styles.aboutMeContainer}>
                     <div className={styles.aboutMeText}>
-                        <p>Emmy heter jag, 26 år gammal  och född i Skellefteå.</p>
-                        <p>Jag flyttade till Umeå 2017 för att studera beteendevetenskap, men bytte 2019 bana till civilingenjör.</p>
+                        <p>Emmy heter jag och är originellt från Skellefteå.  Efter att ha börjat studera beteendevetenskap i Umeå 2017, 
+                            insåg jag mitt verkliga intresse för att lösa logiska problem. 
+                            Därför bytte jag spår och går nu mitt sista år till civilingenjör. 
+                            Jag älskar att skapa digitala lösningar och mjukvaruapplikationer som löser praktiska problem, och samtidigt utmanar min kreativitet.
+                        </p>
                     </div>
                 </div>
                 <div className={styles.smileyContainer}>
