2c16dd91109a43397f4b3a8820376b858adb6104
Emmy Lindgren <emmy.lindgren@hotmail.com>
2023-10-12 20:20:01 +0200

    More cleanup and organizing

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../../App.css';
-import HeroSection from '../sections/HeroSection';
-import EducationSection from '../sections/EducationSection';
-import InfoCardSection from '../sections/InfoCardSection';
+import HeroSection from '../sections/hero/HeroSection';
+import EducationSection from '../sections/education/EducationSection';
+import InfoCardSection from '../sections/education/InfoCardSection';
 import ProjectSection from '../sections/projects/ProjectSection';
-import Footer from '../Footer';
+import Footer from '../sections/footer/Footer';
 import { useEffect } from 'react';
-import AboutMeSection from '../sections/AboutMeSection';
+import AboutMeSection from '../sections/aboutMe/AboutMeSection';
 function Home(){
     useEffect(() => {
         document.title = 'Emmy Lindgren';
