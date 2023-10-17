ac1e7d2da6bde88cc1a704b8021450ad744a4403
Emmy Lindgren <emmy.lindgren@hotmail.com>
2023-10-17 20:38:10 +0200

    Updated already existing projectdetails with new component

--- src/components/pages/Occento.js
+++ src/components/pages/Occento.js
@@ -1,39 +1,57 @@
 import React from 'react';
 import ProjectDetails from '../ProjectDetails';
+import styles from '../ProjectDetails.module.css';
 
 function Occento() {
-    /*Kan lägga alla variabler här, det blir nog snyggast och mest lättläsligt då! */
   return (
     <>
-    <ProjectDetails headline ="OCCENTO"
-      date = 'Sommaren 2021'
-      srcImg_1 ='pictures/OccentoMockupStart.png'
-      introduction ='Mobilspelet som testar dina musikkunskaper! Samla poäng och tävla med dina vänner.'
-
-      standing_images = {['pictures/occentoLat.png','pictures/occentoLaggTillLat.png']}
-      laying_images = {['pictures/OccentoMockupSpel.png']}
-
-      /*assignmentHeadline  as alternative headline for assignmenttext*/
-      assignmenttext = {'Denna app skapades i kursen Utveckling av mobila applikationer som tredje och sista laboration. ' 
-      + 'Uppgiften var väldigt fri, skapa en applikation från egen idé. ' 
-      + 'App/action-bar ska finnas med, tillstånd ska sparas och appen ska använda sig av minst en sensor.'}
-
-      subheadline2 = 'Hur spelar man?'
-
-      subtext2 ={'Appen kommer med några låtar i databasen när den installeras. Du kan välja att spela direkt eller '+
-      'lägga till/ta bort låtar först. När spelaren tryckt på “spela” ska mobilen placeras i pannan och resterande'+
-      ' spelare ska nu sjunga den låttext som dyker upp.  Nu börjar gissningsleken! Gissar du rätt vinklar du mobilen'+
-      ' nedåt och får poäng, vill du passa en låt vinklar du den istället uppåt. En spelomgång är 3 minuter. '}
-
-      /*developmentHeadline  as alternative headline for developmenttext*/
-      developmenttext = {'Först designades gränssnittet med hjälp av Figma. Applikationen implementerades sedan i' +
-      ' Android studio med programmeringsspråket Java. Under hela utvecklingsperioden användes GitHub för versionshantering. '}
-
-      /* Figma, AndroidStudio, Github, Visual Studio Code, Visual Studio, RaspberryPi?, Nåt m SQL? Tänker verktyg här?  */
-      developmenticons ={['icons/FigmaIcon.svg','icons/AndroidStudioIcon.svg','icons/GithubIconPink.svg']}
-    />
-    </>
+      <ProjectDetails 
+          title ="Occento"
+          dateText = 'Sommaren 2021'
+          fieldText = 'Design & implementation'
+          header_images = {['pictures/Occento/Start.png','pictures/Occento/Songs.png','pictures/Occento/Game.png']}
+          header_color = '2'
+      >
+        <div>
+          <p>
+            Mobilspelet som testar dina musikkunskaper! Samla poäng och tävla med dina vänner. 
+          </p>
+        </div>
+        <div>
+          <h3>Vad var uppgiften?</h3>
+          <p>
+          Denna app skapades i kursen Utveckling av mobila applikationer som tredje och sista laboration. 
+          Uppgiften var väldigt fri, skapa en applikation från egen idé. App/action-bar ska finnas med, 
+          tillstånd ska sparas och appen ska använda sig av minst en sensor.
+            </p>
+        </div>
+        <div>
+          <h3>Hur spelar man?</h3>
+          <p>
+          Appen kommer med några låtar i databasen när den installeras. Du kan välja att spela direkt eller lägga till/ta 
+          bort låtar först. När spelaren tryckt på “spela” ska mobilen placeras i pannan och resterande spelare ska nu 
+          sjunga den låttext som dyker upp.  Nu börjar gissningsleken! Gissar du rätt vinklar du mobilen nedåt och får poäng, 
+          vill du passa en låt vinklar du den istället uppåt. En spelomgång är 3 minuter. 
+            </p>
+        </div>
+        <div>
+          <h3>Utvecklingsprocessen</h3>
+          <p>
+            Först designades gränssnittet med hjälp av Figma. 
+            Applikationen implementerades sedan i Android studio i Java. Under hela utvecklingsperioden användes GitHub 
+            för versionshantering. 
+          </p>
+          <div className={styles.icons}>
+            <img src='icons/Figma.svg' alt='figmaIcon'/>
+            <img src='icons/AndroidStudio.svg' alt='androidStudioIcon'/>
+            <img src='icons/Java.svg' alt='JavaIcon'/>
+            <a target="_blank" href="https://github.com/emmylindgren/OCCENTO" rel="noreferrer">
+                <img src='icons/Github.svg' alt='githubIcon'/>
+            </a>
+          </div>
+        </div>
+      </ProjectDetails>
+  </>
   )
 }
-
 export default Occento
